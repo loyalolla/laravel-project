61ebfaccc48370d12055d538560ca8bf695303ca
Olga Kulik <kulik_olla@mail.ru>
2020-08-10 22:42:23 +0300

    new hw

---
+++ resources/views/about.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+    html,
+    body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: 'Nunito', sans-serif;
+        font-weight: 200;
+        height: 100vh;
+        margin: 0;
+    }
+
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 24px;
+    }
+
+    .links>a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="content">
+        <div class="links">
+            <a href="/">Главная</a>
+            <a href="/news">Новости</a>
+            <a href="/about">О проекте</a>
+        </div>
+        <div class="title m-b-md">
+            О проекте
+            <p> Значимость этих проблем настолько очевидна, что реализация намеченного плана развития влечет за собой
+                процесс внедрения и модернизации экономической целесообразности принимаемых решений. Разнообразный и
+                богатый опыт выбранный нами инновационный путь позволяет выполнить важнейшие задания по разработке новых
+                предложений. Таким образом, начало повседневной работы по формированию позиции влечет за собой процесс
+                внедрения и модернизации всесторонне сбалансированных нововведений.
+
+                Разнообразный и богатый опыт реализация намеченного плана развития создаёт предпосылки качественно новых
+                шагов для модели развития?
+
+                Задача организации, в особенности же постоянное информационно-техническое обеспечение нашей деятельности
+                требует от нас системного анализа модели развития. Дорогие друзья, выбранный нами инновационный путь
+                способствует повышению актуальности системы масштабного изменения ряда параметров. Таким образом,
+                дальнейшее развитие различных форм деятельности создаёт предпосылки качественно новых шагов для
+                всесторонне сбалансированных нововведений.
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
